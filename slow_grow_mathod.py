818cdf903dab9c2a6537b63eb51aaedf9211c034
thodorisP <teosfp@hotmail.com>
2024-11-21 10:30:36 -0500

    commit

---
+++ slow_grow_mathod.py
@@ -0,0 +1,92 @@
+import os
+import gzip
+import glob
+import sys
+import pandas as pd
+import numpy as np
+
+def flatten_matrix( matrix ):
+	flat_list = list()
+	for i in matrix:
+		flat_list.extend( i )
+	return flat_list
+
+def get_RUNs():
+		runs = glob.glob( "RUN*" )
+		if runs:
+			return runs
+		else:
+			return []
+
+def get_cc_bm():
+	files = glob.glob( "REPORT*" )
+	if "REPORT" in files:
+		print( "REPORT found" )
+		with open( files[ 0 ], "rb" ) as file:
+			lines = file.readlines()
+		cc = [ line.decode( "utf-8", errors = "ignore" ).strip() for line in lines if b"cc" in line ]	
+		b_m = [ line.decode( "utf-8", errors = "ignore" ).strip() for line in lines if b"b_m" in line ]
+	elif "REPORT.gz" in files:
+		with gzip.open( files[ 0 ], "rb" ) as file:
+			lines = file.readlines()
+		cc = [ line.decode( "utf-8", errors = "ignore" ).strip() for line in lines if b"cc" in line ]	
+		b_m = [ line.decode( "utf-8", errors = "ignore" ).strip() for line in lines if b"b_m" in line ]
+	else:
+		print( "REPORT NOT found" )
+	df_cc = pd.DataFrame( [ row.split() for row in cc ] )
+	df_bm = pd.DataFrame( [ row.split() for row in b_m ] )
+	#print( df_bm.to_string() )
+	return list( df_cc[ 3 ] ), list( df_bm[ 1 ] )
+
+
+def collect_cc_and_bm( path_to_SG_calculation ):
+	if os.path.exists( path_to_SG_calculation ):
+		os.chdir( path_to_SG_calculation )
+		runs = get_RUNs()
+		print( runs )
+		if not runs:
+			CC, B_M = get_cc_bm()
+		else:
+			CC = list()
+			B_M = list()
+			for i in range( 0, len( runs ) ):
+				new_path = path_to_SG_calculation + "/RUN" + str( i + 1 )
+				os.chdir( new_path )
+				cc, b_m = get_cc_bm()
+				CC.append( cc )
+				B_M.append( b_m )
+			os.chdir( path_to_SG_calculation )
+			cc, b_m = get_cc_bm()
+			CC.append( cc )
+			B_M.append( b_m )
+			return [ float( i ) for i in flatten_matrix( CC ) ], [ float( i ) for i in flatten_matrix( B_M ) ]
+	else:
+		print( "Path not found" )
+
+def get_free_energy( path_to_SG_calculation ):
+	tg = [ 0.0 ]
+	cc, b_m = collect_cc_and_bm( path_to_SG_calculation )
+	for i in range( 1, len( cc ) ):
+		gg = 0.5 * ( cc[ i ]  -  cc[ i - 1 ] ) * ( b_m[ i ]  +  b_m[ i - 1 ] )
+		tg.append( tg[ -1 ] + gg )
+	if len( sys.argv ) > 1:
+		start = int( sys.argv[ 1 ] )
+	else:
+		start = 0  
+	if len( sys.argv ) > 2:
+		end = int( sys.argv[ 2 ] ) 
+	else:
+		end = len( cc )  
+	print( start, end )
+
+if __name__ == "__main__":
+	#a, b = parse_report( "REPORT" )
+	path = "/home/theodoros/PROJ_ElectroCat/theodoros/HER/Au/HER_Au/slow_grow_method/tutorial_1/"
+	get_free_energy( path )
+	#cc, b_m = collect_cc_and_bm( path )
+	#print( len( cc ) )
+	#for i in cc:
+	#	print( i )
+	#cc, b_m = get_cc_bm( path )
+	#for i in b_m:
+	#	print( i )

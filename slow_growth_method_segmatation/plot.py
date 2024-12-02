c968f0cd65e004941e32e3d875680ceb80094de4
thodorisP <teosfp@hotmail.com>
2024-12-01 22:06:29 -0500

    commit

--- slow_growth_method_segmatation/plot.py
+++
@@ -1,9 +0,0 @@
-import get_data
-import get_plot
-
-path = "/home/theodoros/PROJ_ElectroCat/theodoros/HER/Au/HER_Au/slow_grow_method/tutorial/"
-
-
-x, y = get_data.get_free_energy( path )
-get_plot.plot_barrier( x, y )
- 

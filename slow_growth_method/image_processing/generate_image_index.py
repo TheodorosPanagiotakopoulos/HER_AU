50b184d9c0ece10e33c997027a9172e7f08a536a
thodorisP <teosfp@hotmail.com>
2025-05-20 07:02:27 -0400

    commit

--- slow_growth_method/image_processing/generate_image_index.py
+++ slow_growth_method/image_processing/generate_image_index.py
@@ -29,36 +29,36 @@ center_atom = 91   #x, y of this atom will be shift to 0,0, it will be at the ce
 
 system = read( loc + '/RUN1/POSCAR' )
 for dir in dirs:
-    with open( dir + '/DATA.js', 'r' ) as f:
-        data = js.load( f )
-    npoints = len( data.keys() )
-    traj = Trajectory( dir + '/MOVIE.traj', 'r' )
-    for i in range( len( traj ) ):
-        count += 1
-        if count > start and count % step == 0:
-            print( count )
-            img = traj[ i ]
-            ref = img[ center_atom ].position * 1.
-            ref[ 1 ] += 1.
-            for at in O_highlight:
-                img[ at ].symbol = 'S'
-            for at in H_shuttle:
-                img[ at ].symbol = 'Ne'
-            for at in H_highlight:
-                img[ at ].symbol = 'He'
-            for at in N_highlight:
-                img[ at ].symbol = 'P' #P
+	with open( dir + '/DATA.js', 'r' ) as f:
+		data = js.load( f )
+	npoints = len( data.keys() )
+	traj = Trajectory( dir + '/MOVIE.traj', 'r' )
+	for i in range( len( traj ) ):
+		count += 1
+		if count > start and count % step == 0:
+			print( count )
+			img = traj[ i ]
+			ref = img[ center_atom ].position * 1.
+			ref[ 1 ] += 1.
+			for at in O_highlight:
+				img[ at ].symbol = 'S'
+			for at in H_shuttle:
+				img[ at ].symbol = 'Ne'
+			for at in H_highlight:
+				img[ at ].symbol = 'He'
+			for at in N_highlight:
+				img[ at ].symbol = 'P' #P
 
-            center = (img.cell[ 0, : ] + img.cell[ 1, : ] ) /2.
-            center[ 2 ] = 0.
-            img.positions[ :, 0 ] -= ref[ 0 ]
-            img.positions[ :, 1 ] -= ref[ 1 ]
-            img.positions[ :, 0 ] += center[ 0 ]
-            img.positions[ :, 1 ] += center[ 1 ]
-            img.wrap()
-            if count in images:
-                prefix = str( count )
-                fout = 'frame_' + prefix
-                if not os.path.isfile( fout + '.png' ):
-                    get_figure_2( img, fout, rot = "180z,-80x,10y", w = 11, h = 11 )
-            os.system( 'rm -f *.ini *.pov' )
+			center = (img.cell[ 0, : ] + img.cell[ 1, : ] ) /2.
+			center[ 2 ] = 0.
+			img.positions[ :, 0 ] -= ref[ 0 ]
+			img.positions[ :, 1 ] -= ref[ 1 ]
+			img.positions[ :, 0 ] += center[ 0 ]
+			img.positions[ :, 1 ] += center[ 1 ]
+			img.wrap()
+			if count in images:
+				prefix = str( count )
+				fout = 'frame_' + prefix
+				if not os.path.isfile( fout + '.png' ):
+					get_figure_2( img, fout, rot = "180z,-80x,10y", w = 11, h = 11 )
+			os.system( 'rm -f *.ini *.pov' )
